c177342aa283bb3d9707b255902dc4e834d1130f
unknown <shreyadhrita@gmail.com>
2022-06-11 00:41:39 +0530

    added changes

--- read_my_blog.py
+++ read_my_blog.py
@@ -4,4 +4,4 @@ from selenium import webdriver
 PATTH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATTH)
 
-driver.get("https://www.youtube.com/")
+driver.get("https://shreyadhrita.blogspot.com/")
